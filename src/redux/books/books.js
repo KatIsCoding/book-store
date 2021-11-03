c89ead272fb3c496d575985a73f5ba8368df992d
KatIsCoding <fabrigoru@gmail.com>
2021-11-03 12:35:56 -0600

    Create Action Creators for add and delete book

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,8 +1,11 @@
+import BooksAPI from './api';
+
+const BooksObj = new BooksAPI();
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 const initState = [];
 
-export const addBook = (payload) => ({
+const addBook = (payload) => ({
   type: ADD_BOOK,
   payload,
 });
@@ -11,6 +14,27 @@ export const removeBook = (payload) => ({
   type: REMOVE_BOOK,
   payload,
 });
+
+export const deleteBook = (id) => (dispatch) => {
+  BooksObj.deleteBook(id).then((response) => {
+    if (response.ok) {
+      dispatch(removeBook(id));
+    }
+  });
+};
+
+export const postNewBook = (book) => (dispatch) => {
+  const requestBook = {
+    item_id: book.id,
+    title: book.title,
+    category: 'None',
+  };
+  BooksObj.postNew(requestBook).then((response) => {
+    if (response.ok) {
+      dispatch(addBook(book));
+    }
+  });
+};
 
 const reducer = (state = initState, action) => {
   switch (action.type) {
