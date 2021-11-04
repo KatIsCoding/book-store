f7a68cfb9807857cc9ff623bd0ec262845887879
KatIsCoding <fabrigoru@gmail.com>
2021-11-04 16:25:39 -0600

    Add Book Form Styling

--- src/components/booksComponents/addBook.js
+++ src/components/booksComponents/addBook.js
@@ -1,26 +1,40 @@
 import { useDispatch } from 'react-redux';
 import { postNewBook } from '../../redux/books/books';
+import './addBook.css';
 
 export default function AddNewBookComponent() {
   const dispatch = useDispatch();
   const handleSubmit = (e) => {
     const bookTitle = e.target.elements.bookTitle.value;
     const bookAuthor = e.target.elements.bookAuthor.value;
+    const category = e.target.elements.category.value;
     const id = Date.now();
     e.target.elements.bookTitle.value = '';
     e.target.elements.bookAuthor.value = '';
-    dispatch(postNewBook({ title: bookTitle, author: bookAuthor, id }));
+    e.target.elements.category.value = '';
+    dispatch(postNewBook({
+      title: bookTitle, author: bookAuthor, id, category,
+    }));
   };
 
   return (
-    <form onSubmit={(e) => {
-      e.preventDefault();
-      handleSubmit(e);
-    }}
-    >
-      <input id="bookTitle" type="text" placeholder="Book Title" />
-      <input id="bookAuthor" type="text" placeholder="Author" />
-      <button type="submit">Add Book</button>
-    </form>
+    <div>
+      <hr styles={{ height: 1, width: '100%' }} />
+      <h2>Add New Book</h2>
+      <form
+        className="form-inline mainForm"
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmit(e);
+        }}
+      >
+        <div className="form-row form-group col-md-6">
+          <input id="bookTitle" className="form-control" type="text" placeholder="Book Title" />
+          <input id="bookAuthor" className="form-control" type="text" placeholder="Author" />
+          <input id="category" className="form-control" type="text" placeholder="Category" />
+          <button type="submit" className="btn btn-primary submitForm">Add Book</button>
+        </div>
+      </form>
+    </div>
   );
 }
